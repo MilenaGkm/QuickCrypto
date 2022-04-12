ea389d15ad2fbad8f1550c05b7cef54175b5f8d3
MilenaGkm <milenagaranin@gmail.com>
2022-04-12 18:30:51 +0300

    added reduxtoolkit/api

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'antd/dist/antd.css';
 // import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import 'antd/dist/antd.css';
+import store from './app/store';
 
 ReactDOM.render(
   // <React.StrictMode>
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   // </React.StrictMode>,
   document.getElementById('root')
